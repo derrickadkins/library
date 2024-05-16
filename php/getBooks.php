70c5e3a322ef0e85f804bf9477ee09f053fd7c29
Derrick Adkins <derrick.l.adkins@gmail.com>
2024-05-16 11:19:08 -0400

    Update getBooks.php
    update query to show available if never checked out

--- php/getBooks.php
+++ php/getBooks.php
@@ -3,7 +3,7 @@
 include '../db/db_connect.php';
 
 $books_sql = "
-    SELECT Books.*, LatestCheckouts.CheckedInDate IS NULL AS CheckedOut
+    SELECT Books.*, IF(LatestCheckouts.BookID IS NULL, 0, LatestCheckouts.CheckedInDate IS NULL) AS CheckedOut
     FROM Books
     LEFT JOIN (
         SELECT Checkouts.BookID, Checkouts.CheckedInDate
@@ -15,6 +15,7 @@ $books_sql = "
         ) as MaxCheckouts ON Checkouts.BookID = MaxCheckouts.BookID AND Checkouts.CheckedOutDate = MaxCheckouts.MaxCheckedOutDate
     ) as LatestCheckouts ON Books.BookID = LatestCheckouts.BookID
 ";
+
 $books_result = $conn->query($books_sql);
 $books = array();
 while($row = $books_result->fetch_assoc()) {
